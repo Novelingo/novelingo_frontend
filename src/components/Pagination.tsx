7ca935da8eaa25841d98ce9b2af8565a6718a4b1
nasrinette <97260770+nasrinette@users.noreply.github.com>
2023-09-28 14:33:20 +0400

    Pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+interface PaginationProps {
+  totalPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ totalPage, onPageChange }) => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= totalPage) {
+      setCurrentPage(page);
+      onPageChange(page);
+    }
+  };
+
+  const renderPageNumbers = () => {
+    const pages = [];
+
+    if (totalPage <= 5) {
+      for (let i = 1; i <= totalPage; i++) {
+        pages.push(i);
+      }
+    } else {
+      if (currentPage <= 3) {
+        pages.push(1, 2, 3, 4, '...', totalPage);
+      } else if (currentPage > totalPage - 3) {
+        pages.push(1, '...', totalPage - 3, totalPage - 2, totalPage - 1, totalPage);
+      } else {
+        pages.push(1, '...', currentPage - 1, currentPage, currentPage + 1, '...', totalPage);
+      }
+    }
+
+    return pages.map((page, index) => {
+      if (typeof page === 'number') {
+        return (
+          <button
+            key={index}
+            className={`w-8 h-8 text-white outline outline-white outline-2 flex justify-center items-center m-2  ${
+              page === currentPage ? 'outline outline-pink text-white' : ''
+            }`}
+            onClick={() => handlePageChange(page)}
+          >
+            {page}
+          </button>
+        );
+      } else {
+        return (
+          <span key={index} className="w-10 h-10 flex justify-center items-center m-2">
+            {page}
+          </span>
+        );
+      }
+    });
+  };
+
+  return (
+    <div className="text-white flex justify-center items-center mt-4">
+      <button
+        className="w-8 h-8 outline outline-white outline-2 flex justify-center items-center m-2 "
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        «
+      </button>
+
+      {renderPageNumbers()}
+
+      <button
+        className="w-8 h-8 outline outline-white outline-2 flex justify-center items-center m-2"
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === totalPage}
+      >
+        »
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/Novels.tsx
+++ src/pages/Novels.tsx
@@ -2,13 +2,20 @@ import NovelCard from "../components/NovelCard";
 import bookcover from "../assets/bookcover.png";
 import Filter from "../components/Filter";
 import { useGetNovelsQuery } from "../apis/novelApi";
+import Pagination from '../components/Pagination';
+import { useState } from 'react';
+
+
 
 export default function Novels() {
 
-  const { data: { data: novels } = { data: [] } } = useGetNovelsQuery({
-    page: 1,
-    limit: 12,
-  });
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { data: { data: novels, pageTotal } = { data: [], pageTotal: 0 } } = useGetNovelsQuery({
+    page: currentPage,
+    limit: 9,
+});
+
   // const filters = Array.from({ length: 10 }).map((_, index) => ({
   //   title: `Filter ${index} `,
   //   options: [
@@ -48,9 +55,10 @@ export default function Novels() {
 
   ]
 
+
+
   return (
     <div className="mt-10 md:px-20 px-5 md:py-20 py-10 flex flex-col justify-center ">
-      
       <div className="fixed top-16 left-1/2 w-full max-w-screen-xl transform -translate-x-1/2 px-16 2xl:p-0 pt-4 bg-gradient-to-r from-dark1 to-dark2 z-10">
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -80,8 +88,8 @@ export default function Novels() {
         </div>
       </div>
 
-      <div className="mt-6 flex flex-wrap justify-center ">
-        <div className="pr-24">
+      <div className="mt-6 md:px-10 px-5 py-10  justify-center  flex">
+        <div className="">
           {filters.map((filter) => (
             <Filter
               key={filter.title}
@@ -91,7 +99,7 @@ export default function Novels() {
           ))}
         </div>
 
-        <div className="w-3/4 flex flex-wrap justify-end">
+        <div className="flex flex-wrap justify-end">
           {novels.map((novel) => (
             <NovelCard
               key={novel.id}
@@ -102,8 +110,17 @@ export default function Novels() {
               cover={bookcover}
             />
           ))}
+          
         </div>
+        
       </div>
+ 
+          <Pagination 
+      totalPage ={pageTotal} 
+      onPageChange={(page) => setCurrentPage(page)}
+    />
+     
+    
     </div>
   );
 }
