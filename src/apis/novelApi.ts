36bdb9733994f8e2e40988ce7aab75cf7fef0ee7
MahammadNuriyev62 <maganuriyev@gmail.com>
2023-11-06 22:38:13 +0400

    added filtration

--- src/apis/novelApi.ts
+++ src/apis/novelApi.ts
@@ -2,45 +2,68 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Novel, NovelFilter } from "../types/novel.type";
 
 interface GetNovelsResponse {
-  data: Omit<Novel, "paragraphs">[];
-  filter: {
-    groups: NovelFilter[];
-  };
-  countTotal: number;
-  countCurrent: number;
-  pageTotal: number;
-  pageCurrent: number;
+	data: Omit<Novel, "paragraphs">[];
+	filter: {
+		groups: NovelFilter[];
+	};
+	countTotal: number;
+	countCurrent: number;
+	pageTotal: number;
+	pageCurrent: number;
 }
 
 interface GetNovelByIdResponse {
-  data: Novel;
+	data: Novel;
+}
+
+export interface GetNovelsRequest {
+	page: number;
+	limit: number;
+	filters: { [key: string]: string[] };
 }
 
 // Define a service using a base URL and expected endpoints
 export const novelApi = createApi({
-  reducerPath: "novelApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://novelingo-o2cuk.ondigitalocean.app/api/novels/",
-  }),
-  endpoints: (builder) => ({
-    getNovels: builder.query<
-      GetNovelsResponse,
-      { page: number; limit: number }
-    >({
-      query: ({ page, limit }) => {
-        return {
-          url: "/",
-          params: {
-            page,
-            limit,
-          },
-        };
-      },
-    }),
-    getNovelById: builder.query<GetNovelByIdResponse, string>({
-      query: (id) => `${id}`,
-    }),
-  }),
+	reducerPath: "novelApi",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "http://127.0.0.1:8000/api/novels/",
+		paramsSerializer: (params) => {
+			// add support for array params
+			const searchParams = new URLSearchParams();
+			Object.entries(params).forEach(([key, value]) => {
+				if (Array.isArray(value)) {
+					value.forEach((item) => searchParams.append(key, item));
+				} else {
+					searchParams.append(key, value);
+				}
+			});
+			return searchParams.toString();
+		},
+	}),
+	endpoints: (builder) => ({
+		getNovels: builder.query<GetNovelsResponse, GetNovelsRequest>({
+			query: ({ page, limit, filters }) => {
+				console.log({
+					// query params
+					page,
+					limit,
+					...filters,
+				});
+				return {
+					url: "/",
+					params: {
+						// query params
+						page,
+						limit,
+						...filters,
+					},
+				};
+			},
+		}),
+		getNovelById: builder.query<GetNovelByIdResponse, string>({
+			query: (id) => `${id}`,
+		}),
+	}),
 });
 
 export const { useGetNovelsQuery, useGetNovelByIdQuery } = novelApi;
