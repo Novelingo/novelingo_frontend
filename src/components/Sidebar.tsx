02cea737b1741dee9e3f4f1c7ac2006a99d298d4
MahammadNuriyev62 <maganuriyev@gmail.com>
2023-11-07 01:28:07 +0400

    temporarly commented filter in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,63 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import { FaChevronDown, FaChevronRight, FaTimes } from 'react-icons/fa';
-import Filter from './Filter';
+import React, { useState, useEffect } from "react";
+import { FaChevronDown, FaChevronRight, FaTimes } from "react-icons/fa";
+import Filter from "./Filter";
 
 interface Filter {
-  title: string;
-  options: { id: string, title: string }[];
+	title: string;
+	options: { id: string; title: string }[];
 }
 
 interface SidebarProps {
-  filters: Filter[];
-  onClose: () => void;
+	filters: Filter[];
+	onClose: () => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ filters, onClose }) => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const [isVisible, setIsVisible] = useState<boolean>(false);  // State to track visibility
+	const [activeIndex, setActiveIndex] = useState<number | null>(null);
+	const [isVisible, setIsVisible] = useState<boolean>(false); // State to track visibility
 
-  useEffect(() => {
-    setIsVisible(true);  // Set the sidebar to be visible when the component mounts
-  }, []);
+	useEffect(() => {
+		setIsVisible(true); // Set the sidebar to be visible when the component mounts
+	}, []);
 
-  const toggleFilter = (index: number) => {
-    if (activeIndex === index) {
-      setActiveIndex(null);
-    } else {
-      setActiveIndex(index);
-    }
-  };
+	const toggleFilter = (index: number) => {
+		if (activeIndex === index) {
+			setActiveIndex(null);
+		} else {
+			setActiveIndex(index);
+		}
+	};
 
-  const handleClose = () => {
-    setIsVisible(false);  // Animate the sidebar out
-    setTimeout(onClose, 300);  // Call the provided onClose function after the animation duration
-  };
+	const handleClose = () => {
+		setIsVisible(false); // Animate the sidebar out
+		setTimeout(onClose, 300); // Call the provided onClose function after the animation duration
+	};
 
-  return (
-    <div className={`fixed z-30 top-0 left-0 h-full w-64 bg-light rounded-md text-dark1 transform transition-transform duration-300 ease-in-out ${isVisible ? 'translate-x-0' : '-translate-x-full'}`}>
-      <button onClick={handleClose} className="absolute top-4 right-4 text-2xl">
-        <FaTimes />
-      </button>
-      <div className='mt-6'>
-        {filters.map((filter, index) => (
-          <div key={index} className="mb-2">
-            <div
-              onClick={() => toggleFilter(index)}
-              className="cursor-pointer px-4 py-2 flex items-center"
-            >
-              {filter.options && activeIndex === index ? <FaChevronDown /> : <FaChevronRight />}
-              <span className="ml-2">{filter.title}</span>
-            </div>
-            {activeIndex === index && filter.options && (
+	return (
+		<div
+			className={`fixed z-30 top-0 left-0 h-full w-64 bg-light rounded-md text-dark1 transform transition-transform duration-300 ease-in-out ${
+				isVisible ? "translate-x-0" : "-translate-x-full"
+			}`}
+		>
+			<button onClick={handleClose} className="absolute top-4 right-4 text-2xl">
+				<FaTimes />
+			</button>
+			<div className="mt-6">
+				{filters.map((filter, index) => (
+					<div key={index} className="mb-2">
+						<div
+							onClick={() => toggleFilter(index)}
+							className="cursor-pointer px-4 py-2 flex items-center"
+						>
+							{filter.options && activeIndex === index ? (
+								<FaChevronDown />
+							) : (
+								<FaChevronRight />
+							)}
+							<span className="ml-2">{filter.title}</span>
+						</div>
+						{/* {activeIndex === index && filter.options && (
               <div className="pl-4 text-sm">
                 <Filter options={filter.options} />
               </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            )} */}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
