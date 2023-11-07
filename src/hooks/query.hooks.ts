e63030c960b4f749263293c670723b4b70aecc29
MahammadNuriyev62 <maganuriyev@gmail.com>
2023-11-08 00:09:29 +0400

    more readable query params url

--- src/hooks/query.hooks.ts
+++ src/hooks/query.hooks.ts
@@ -1,17 +1,29 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, createSearchParams } from "react-router-dom";
 import { useEffect, useReducer } from "react";
-import { extractJSON } from "../utils";
+import queryString from "query-string";
 
-export const useQueryReducer = <T>(defaultValue: T, key = "s") => {
+export const useQueryReducer = <T extends Record<string, number | string[]>>(
+  defaultValue: T,
+  key = "s"
+) => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const [state, dispatch] = useReducer(
     (state: T, newState: Partial<T>) => ({ ...state, ...newState } as T),
-    extractJSON<T>(searchParams.get(key) || "", defaultValue)
+    searchParams.toString()
+      ? (queryString.parse(decodeURI(searchParams.toString()), {
+          parseNumbers: true,
+          arrayFormat: "index",
+        }) as T)
+      : defaultValue
   );
 
+  console.log({ state }, decodeURI(searchParams.toString()));
+
   useEffect(() => {
-    setSearchParams({ [key]: JSON.stringify(state) });
+    setSearchParams(
+      createSearchParams(queryString.stringify(state, { arrayFormat: "index" }))
+    );
   }, [state, setSearchParams, key]);
 
   return [state, dispatch] as const;
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function extractJSON<T>(str: string, defaultValue: T): T {
-  try {
-    return JSON.parse(str) as T;
-  } catch (e) {
-    return defaultValue;
-  }
-}
