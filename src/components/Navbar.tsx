8c19b7133a9702fb47db45b65e6ec910a6a06853
nasrinette <97260770+nasrinette@users.noreply.github.com>
2023-11-17 13:06:49 +0400

    menu transtion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             open
               ? "top-12 mt-4 pl-12 bg-gradient-to-r from-dark1 to-dark2"
               : "md:bg-transparent top-[-490px]"
-          } md:flex md:items-center md:pb-0 pb-12 absolute md:static left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-9 transition-all duration-500 ease-in `}
+          } md:flex md:items-center md:pb-0 pb-12 absolute md:static left-0 w-full md:w-auto mt-4 md:mt-0 md:pl-9 transition-all duration-200 ease-in `}
         >
           {links.map((link) => (
             <li key={link.name} className="md:ml-8 text-lg md:my-0 my-7">
