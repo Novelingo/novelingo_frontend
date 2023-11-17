862209c06add901a15e5725336ce7eabe0ee2342
nasrinette <97260770+nasrinette@users.noreply.github.com>
2023-11-17 13:56:20 +0400

    Add SearchBar component to Novels page

--- src/pages/Novels.tsx
+++ src/pages/Novels.tsx
@@ -13,6 +13,7 @@ import AppButton from "../components/AppButton";
 import Loading from "../components/Loading";
 
 import { useQueryReducer } from "../hooks/query.hooks";
+import SearchBar from "../components/SearchBar";
 
 type IState = { [key: string]: string[] } & { page: number };
 
@@ -44,32 +45,7 @@ export default function Novels() {
   return (
     <div className="mt-3 md:px-20 px-5 md:py-20 py-10 flex flex-col justify-center">
       <div className=" fixed top-16 left-1/2 w-full z-20 md:z-30 max-w-screen-xl transform -translate-x-1/2 px-16 2xl:p-0 pt-3 bg-gradient-to-r from-dark1 to-dark2 ">
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-purple"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </div>
-          <input
-            type="search"
-            id="default-search"
-            className="w-full text-purple p-3 pl-10 placeholder:text-purple md:text-base bg-light rounded shadow-sm focus:outline-none text-sm"
-            placeholder="Search novels"
-            required
-          />
-        </div>
+        <SearchBar />
       </div>
 
       <div className="mt-6 py-10 flex flex-row  justify-center relative overflow-hidden ">
